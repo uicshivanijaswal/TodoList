f9dce20103915795efa0a4b5ad9d832ce8897e7d
Shivani <uic.shivanijaswal@gmail.com>
2024-11-19 12:31:50 +0000

    Final commit1

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,21 @@
-#!/usr/bin/env bash 
+#!/usr/bin/env bash
+# Check if there is instance running with the image name we are deploying
 CURRENT_INSTANCE=$(sudo docker ps -a -q --filter ancestor="$IMAGE_NAME" --format="{{.ID}}")
 
+# If an instance does exist stop the instance
 if [ "$CURRENT_INSTANCE" ]
 then
-    sudo docker rm ${sudo docker stop $CURRENT_INSTANCE}
+  sudo docker rm $(sudo docker stop $CURRENT_INSTANCE)
 fi
 
+# Pull down the instance from dockerhub
 sudo docker pull $IMAGE_NAME
 
+# Check if a docker container exists with the name of $CONTAINER_NAME if it does remove the container
 CONTAINER_EXISTS=$(sudo docker ps -a | grep $CONTAINER_NAME)
 if [ "$CONTAINER_EXISTS" ]
 then
-    sudo docker rm $CONTAINER_NAME 
+  sudo docker rm $CONTAINER_NAME
 fi
 
 sudo docker run -p 3000:3000 -d --name $CONTAINER_NAME $IMAGE_NAME
